e7ff8f0e22e19d3d36b060ebf81bf7f0633abbe0
flymanflyman <406544970@qq.com>
2019-03-26 19:16:21 +0800

    26.3

--- frmMain.cs
+++ frmMain.cs
@@ -30,7 +30,7 @@ namespace DevExpress.XtraBars.Demos.MDIDemo
         }
         private string mySkinName;
         private const string _Text = "myBatis 粘子层代码生成器";
-        private const string _Version = "1.0";
+        private const string _Version = "0.1";
         bool IsTabbedMdi { get { return biTabbedMDI.Down; } }
         public static string Version => _Version;
 
