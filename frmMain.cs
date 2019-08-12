e0541d6a4cd20a29f37743ae2de2be522c94203a
Administrator <406544970@qq.com>
2019-08-12 15:53:26 +0800

    20190812.3

--- frmMain.cs
+++ frmMain.cs
@@ -510,7 +510,7 @@ namespace DevExpress.XtraBars.Demos.MDIDemo
                     class_WindowType = xtra.MdiChild.Tag as Class_WindowType;
                     if (class_WindowType != null)
                     {
-                        if (class_WindowType.XmlFileName == ActiveMDIForm.Tag.ToString())
+                        if (class_WindowType.XmlFileName == (ActiveMDIForm.Tag as Class_WindowType).XmlFileName)
                             class_WindowType.ActiveSign = true;
                         class_WindowTypes.Add(class_WindowType);
                     }
